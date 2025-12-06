0f249c29a222b1e6fe4606f0c8b215de4b36a215
Anton Sychev <antonio_sychev@mail.ru>
2025-12-06 20:06:54 +0300

    Создание массива длиной 20-100, заполненного случайными числами от -100 до 100

--- RandomNumbersSortedRandomly/app.ts
+++ RandomNumbersSortedRandomly/app.ts
@@ -1,1 +1,16 @@
-console.log('Hello world123')
+const arrayLength = randomIntFromInterval(20, 100);
+let array = new Array<number>(arrayLength);
+console.log("Случайный массив:");
+for (let i = 0; i < arrayLength; i++) {
+    array[i] = randomIntFromInterval(-100, 100);
+    console.log(array[i]);
+}
+
+
+
+
+
+
+function randomIntFromInterval(min: number, max: number): number { 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
--- RandomNumbersSortedRandomly/dist/app.js
+++ RandomNumbersSortedRandomly/dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('Hello world123');
+const arrayLength = randomIntFromInterval(20, 100);
+let array = new Array(arrayLength);
+console.log("��������� ������:");
+for (let i = 0; i < arrayLength; i++) {
+    array[i] = randomIntFromInterval(-100, 100);
+    console.log(array[i]);
+}
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 //# sourceMappingURL=app.js.map
